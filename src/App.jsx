157a1923e05634ada4cd30c043e51aedfa0901a1
Haram Kwon <kharam1436@icloud.com>
2024-03-20 12:26:19 -0700

    basic feature added

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,44 @@ const choice = {
 
 function App() {
   const [userSelect, setUserSelect] = useState(choice["paper"]);
+  const [computerChoice, setComputerChoice] = useState(choice["paper"]);
+  const [result, setResult] = useState({});
+
+  const judgement = (user, computer) => {
+    console.log(user, computer);
+    if (user === computer) return { user: "tie", computer: "tie" };
+    if (
+      (user === "scissors" && computer === "paper") ||
+      (user === "rock" && computer === "scissors") ||
+      (user === "paper" && computer === "rock")
+    ) {
+      return { user: "win", computer: "lose" };
+    } else {
+      return { user: "lose", computer: "win" };
+    }
+  };
+
+  const randomChoice = () => {
+    const select = Object.keys(choice);
+    const random = Math.floor(Math.random() * select.length);
+    return choice[select[random]];
+  };
 
   const play = (userChoice) => {
-    setUserSelect(choice[userChoice]);
+    const user = choice[userChoice];
+    const computer = randomChoice();
+    setUserSelect(user);
+    setComputerChoice(computer);
+    setResult(judgement(user.name, computer.name));
   };
 
   return (
     <>
-      <div className='main'>
-        <Box title='You' userSelect={userSelect} />
-        <Box title='Computer' userSelect={userSelect} />
+      <div className="main">
+        <Box title="You" item={userSelect} result={result.user} />
+        <Box title="Computer" item={computerChoice} result={result.computer} />
       </div>
-      <div className='main'>
+      <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
         <button onClick={() => play("rock")}>바위</button>
         <button onClick={() => play("paper")}>보</button>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Box = ({ title, userSelect }) => {
+export const Box = ({ title, item, result }) => {
   return (
-    <div className='box'>
+    <div className={`box ${result}`}>
       <h1>{title}</h1>
-      <img src={userSelect.img} alt={userSelect.name} className='item-img' />
-      <h2>Win</h2>
+      <img src={item.img} alt={item.name} className="item-img" />
+      <h2>{result}</h2>
     </div>
   );
 };
